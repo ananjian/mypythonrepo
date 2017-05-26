a795e0cf6ff427d1ead856abd518aff1bd3dcf17
ananjian <ashish.nanjiani@gmail.com>
2017-05-26 15:36:55 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+fname=raw_input("Enter file name: ")
+fh=open(fname)
+lst= list()
+for line in fh:
+    words=line.split()
+    for w in words:
+        if w not in lst:
+            lst.append(w)
+        #return lst
+newlist = sorted(lst)
+print newlist
+
+    
+
+
+
